c0aefdddfe41a2b0e2046338ce24b1102e965e03
mindae <MindaeLaddoo01>
2024-04-06 21:35:18 +0530

    init commit

---
+++ src/test/java/com/mindae/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindae.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
